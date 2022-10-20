06a4a61c5b466829cdd3eb06866e448ddc58bd0e
Naoyuki Ishida <39.ishida@gmail.com>
2022-10-20 00:11:50 -0700

    Add item request spec

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ItemsController < ApplicationController
   def index
     @search = Item.ransack(params[:q])
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,11 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Items", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/items/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Items', type: :request do
+  describe 'GET /index' do
+    context 'when user is logged in' do
+      it 'returns http success' do
+        user = create(:user)
+        sign_in user
+        get items_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when user is not logged in' do
+      it 'returns http success' do
+        get items_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
-
 end
