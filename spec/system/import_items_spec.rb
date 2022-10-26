8e0e41bc86bd06fc57a2db02012781bd8b1ebe11
Naoyuki Ishida <39.ishida@gmail.com>
2022-10-26 14:19:54 -0700

    WIP system spec for import items

--- spec/system/import_items_spec.rb
+++ spec/system/import_items_spec.rb
@@ -8,7 +8,20 @@ RSpec.describe 'ImportItems', type: :system do
   end
 
   scenario 'A user log in and import and update item master' do
-    item1 = create(:item)
-    item2 = create(:item)
+    vendor = create(:vendor)
+    user = create(:user)
+
+    visit root_path
+    click_link 'Sign in'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+
+    click_link 'Item Info'
+    file_path = Rails.root.join('spec', 'fixtures', 'item1.xlsx')
+    attach_file('file', file_path)
+    click_on 'Upload'
+
+    expect(page).to have_content 'ANI10001'
   end
 end
