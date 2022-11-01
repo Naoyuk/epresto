99422e369629ed7c4a57e4a82f9285df41b98c0b
Naoyuki Ishida <39.ishida@gmail.com>
2022-11-01 12:03:32 -0700

    Update item import system spec

--- spec/system/import_items_spec.rb
+++ spec/system/import_items_spec.rb
@@ -22,6 +22,28 @@ RSpec.describe 'ImportItems', type: :system do
     attach_file('file', file_path)
     click_on 'Upload'
 
-    expect(page).to have_content 'ANI10001'
+    expect(page.all(".item-code")[0]).to have_content 'ANI40002'
+    expect(page.all(".pack")[0]).to have_content '6'
+    expect(page.all(".item-code")[1]).to have_content 'ANI40001'
+    expect(page.all(".pack")[1]).to have_content '12'
+    expect(page.all(".item-code")[2]).to have_content 'ANI40000'
+    expect(page.all(".pack")[2]).to have_content '1'
+    expect(page.all(".stock")[2]).to have_content '0'
+    expect(page.all(".item-code")[7]).to have_content 'ANI12020'
+    expect(page.all(".z-price")[7]).to have_content '21.28'
+
+    file_path = Rails.root.join('spec', 'fixtures', 'item2.xlsx')
+    attach_file('file', file_path)
+    click_on 'Upload'
+
+    expect(page.all(".item-code")[0]).to have_content 'ANI40002'
+    expect(page.all(".pack")[0]).to have_content '100'
+    expect(page.all(".item-code")[1]).to have_content 'ANI40001'
+    expect(page.all(".pack")[1]).to have_content '200'
+    expect(page.all(".item-code")[2]).to have_content 'ANI40000'
+    expect(page.all(".pack")[2]).to have_content '100'
+    expect(page.all(".stock")[2]).to have_content '1'
+    expect(page.all(".item-code")[7]).to have_content 'ANI12020'
+    expect(page.all(".z-price")[7]).to have_content '100.12'
   end
 end
