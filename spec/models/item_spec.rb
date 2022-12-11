f9d3f803d87cebe45e58021d95c2e3e468b56b1e
Naoyuki Ishida <39.ishida@gmail.com>
2022-12-11 00:38:53 -0800

    Item#importのテスト
    item/_item.html.erbのclass名やカラム名を修正
    Itemのitem_codeやupc、asinはnil許可に変更したのでmodels/item_specのvalidationテストを削除
    system/import_items_spec.rbを修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,24 +7,6 @@ RSpec.describe Item, type: :model do
     expect(build(:item)).to be_valid
   end
 
-  it 'is invalid without item_code' do
-    item = build(:item, item_code: nil)
-    expect(item).not_to be_valid
-    expect(item.errors[:item_code]).to include("can't be blank")
-  end
-
-  it 'is invalid without upc' do
-    item = build(:item, upc: nil)
-    expect(item).not_to be_valid
-    expect(item.errors[:upc]).to include("can't be blank")
-  end
-
-  it 'is invalid without ASIN' do
-    item = build(:item, asin: nil)
-    expect(item).not_to be_valid
-    expect(item.errors[:asin]).to include("can't be blank")
-  end
-
   describe 'case or each' do
     before do
       @item_case = create(:item, case: true)
--- spec/system/import_items_spec.rb
+++ spec/system/import_items_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'ImportItems', type: :system do
     driven_by(:rack_test)
   end
 
-  scenario 'An admin-user log in and import and update item master' do
+  scenario 'An admin-user log in and import files and update item master' do
     create(:vendor)
     user = create(:user, sysadmin: true)
 
@@ -18,50 +18,94 @@ RSpec.describe 'ImportItems', type: :system do
     click_button 'Log in'
 
     click_link 'Item Master'
-    file_path = Rails.root.join('spec', 'fixtures', 'item1.xlsx')
+    file_path = Rails.root.join('spec', 'fixtures', 'Catalog.xlsm')
     attach_file('file', file_path)
     click_on 'Upload'
 
-    expect(page.all('.item-code')[0]).to have_content 'ANI40002'
-    expect(page.all('.pack')[0]).to have_content '6'
-    expect(page.all('.item-code')[1]).to have_content 'ANI40001'
-    expect(page.all('.pack')[1]).to have_content '12'
-    expect(page.all('.item-code')[2]).to have_content 'ANI40000'
-    expect(page.all('.pack')[2]).to have_content '1'
-    expect(page.all('.item-code')[7]).to have_content 'ANI12020'
-    expect(page.all('.z-price')[7]).to have_content '21.28'
+    index_last = Item.all.count - 1
 
-    # For checking to be updated or passed
-    before_updated_at10001 = Item.find_by(item_code: 'ANI10001').updated_at
-    before_updated_at10120 = Item.find_by(item_code: 'ANI10120').updated_at
-    before_updated_at11300 = Item.find_by(item_code: 'ANI11300').updated_at
-    before_updated_at12005 = Item.find_by(item_code: 'ANI12005').updated_at
-    before_updated_at40001 = Item.find_by(item_code: 'ANI40001').updated_at
-    before_updated_at40002 = Item.find_by(item_code: 'ANI40002').updated_at
+    expect(page.all('.asin')[0]).to have_content 'B0SSSSSSMD'
+    expect(page.all('.model-number')[0]).to have_content 'ZZZ02017-CASE'
+    expect(page.all('.upc')[0]).to have_content '832938035370'
+    expect(page.all('.external-product-id')[0]).to have_content '832938035370'
+    expect(page.all('.external-product-id-type')[0]).to have_content 'UPC'
 
-    file_path = Rails.root.join('spec', 'fixtures', 'item2.xlsx')
+    expect(page.all('.asin')[1]).to have_content 'B0UUUUUUFB'
+    expect(page.all('.model-number')[1]).to have_content 'GGG00039'
+    expect(page.all('.upc')[1]).to have_content '844147004345'
+    expect(page.all('.external-product-id')[1]).to have_content '844147004345'
+    expect(page.all('.external-product-id-type')[1]).to have_content 'UPC'
+
+    expect(page.all('.asin')[2]).to have_content 'B0FFFFFFG3'
+    expect(page.all('.model-number')[2]).to have_content 'FFF00006-Unit'
+    expect(page.all('.ean')[2]).to have_content '8216238202251'
+    expect(page.all('.external-product-id')[2]).to have_content '8216238202251'
+    expect(page.all('.external-product-id-type')[2]).to have_content 'EAN'
+
+    expect(page.all('.asin')[6]).to have_content 'B0BBBBBBZC'
+    expect(page.all('.model-number')[6]).to have_content 'BBB00002-CASE'
+    expect(page.all('.gtin')[6]).to have_content '10895079021941'
+    expect(page.all('.external-product-id')[6]).to have_content '10895079021941'
+    expect(page.all('.external-product-id-type')[6]).to have_content 'GTIN'
+
+    expect(page.all('.asin')[index_last]).to have_content 'B0AAAAAAWX'
+    expect(page.all('.model-number')[index_last]).to have_content 'AAA00001-Unit'
+    expect(page.all('.upc')[index_last]).to have_content '815094021444'
+    expect(page.all('.external-product-id')[index_last]).to have_content '815094021444'
+    expect(page.all('.external-product-id-type')[index_last]).to have_content 'UPC'
+    
+    records_amount = Item.all.count
+
+    file_path = Rails.root.join('spec', 'fixtures', 'Access_item.xlsx')
     attach_file('file', file_path)
     click_on 'Upload'
 
-    expect(page.all('.item-code')[0]).to have_content 'ANI40002'
-    expect(page.all('.pack')[0]).to have_content '100'
-    expect(page.all('.item-code')[1]).to have_content 'ANI40001'
-    expect(page.all('.pack')[1]).to have_content '12'
-    expect(page.all('.item-code')[2]).to have_content 'ANI40000'
-    expect(page.all('.pack')[2]).to have_content '100'
-    expect(page.all('.item-code')[7]).to have_content 'ANI12020'
-    expect(page.all('.z-price')[7]).to have_content '100.12'
-    after_updated_at10001 = Item.find_by(item_code: 'ANI10001').updated_at
-    after_updated_at10120 = Item.find_by(item_code: 'ANI10120').updated_at
-    after_updated_at11300 = Item.find_by(item_code: 'ANI11300').updated_at
-    after_updated_at12005 = Item.find_by(item_code: 'ANI12005').updated_at
-    after_updated_at40001 = Item.find_by(item_code: 'ANI40001').updated_at
-    after_updated_at40002 = Item.find_by(item_code: 'ANI40002').updated_at
-    expect(before_updated_at10001).to eq after_updated_at10001
-    expect(before_updated_at10120).not_to eq after_updated_at10120
-    expect(before_updated_at11300).to eq after_updated_at11300
-    expect(before_updated_at12005).not_to eq after_updated_at12005
-    expect(before_updated_at40001).to eq after_updated_at40001
-    expect(before_updated_at40002).not_to eq after_updated_at40002
+    index_last = Item.all.count - 1
+
+    expect(page.all('.asin')[0]).to have_content 'B0SSSSSSMD'
+    expect(page.all('.model-number')[0]).to have_content 'ZZZ02017-CASE'
+    expect(page.all('.upc')[0]).to have_content '832938035370'
+    expect(page.all('.external-product-id')[0]).to have_content '832938035370'
+    expect(page.all('.external-product-id-type')[0]).to have_content 'UPC'
+    expect(page.all('.item-code')[0]).to have_content 'AAC00217'
+    expect(page.all('.title')[0]).to have_content 'Sample, 6 Count of 500g'
+
+    expect(page.all('.asin')[1]).to have_content 'B0UUUUUUFB'
+    expect(page.all('.model-number')[1]).to have_content 'GGG00039'
+    expect(page.all('.upc')[1]).to have_content '844147004345'
+    expect(page.all('.external-product-id')[1]).to have_content '844147004345'
+    expect(page.all('.external-product-id-type')[1]).to have_content 'UPC'
+    expect(page.all('.item-code')[1]).to have_content 'AAC00216'
+    expect(page.all('.title')[1]).to have_content 'Sample 1 count'
+
+    expect(page.all('.asin')[2]).to have_content 'B0FFFFFFG3'
+    expect(page.all('.model-number')[2]).to have_content 'FFF00006-Unit'
+    expect(page.all('.ean')[2]).to have_content '8216238202251'
+    expect(page.all('.external-product-id')[2]).to have_content '8216238202251'
+    expect(page.all('.external-product-id-type')[2]).to have_content 'EAN'
+    expect(page.all('.item-code')[2]).to have_content 'AAC00213'
+    expect(page.all('.title')[2]).to have_content 'CDE, YYY Dressing, 200ml (Pack of 1)'
+
+    expect(page.all('.asin')[6]).to have_content 'B0BBBBBBZC'
+    expect(page.all('.model-number')[6]).to have_content 'BBB00002-CASE'
+    expect(page.all('.gtin')[6]).to have_content '10895079021941'
+    expect(page.all('.external-product-id')[6]).to have_content '10895079021941'
+    expect(page.all('.external-product-id-type')[6]).to have_content 'GTIN'
+    expect(page.all('.item-code')[6]).to have_content 'AAC00207'
+    expect(page.all('.title')[6]).to have_content 'ABC Foods, XXX Dressing, 6 Count of 237ml'
+
+    expect(page.all('.asin')[index_last]).to have_content 'B0AAAAAAWX'
+    expect(page.all('.model-number')[index_last]).to have_content 'AAA00001-Unit'
+    expect(page.all('.upc')[index_last]).to have_content '815094021444'
+    expect(page.all('.external-product-id')[index_last]).to have_content '815094021444'
+    expect(page.all('.external-product-id-type')[index_last]).to have_content 'UPC'
+    # IM_CASE_UPC != external_product_id のバターン
+    expect(page.all('.item-code')[index_last]).to have_content ''
+    expect(page.all('.title')[index_last]).to have_content ''
+
+    # Catalogファイルに無いItemはインポートされていない
+    expect(page.all('.item-code')).not_to have_content 'XXXXXXXX'
+    expect(page.all('.title')).not_to have_content 'Sample XXXXX'
+    expect(Item.all.count).to eq records_amount
   end
 end
