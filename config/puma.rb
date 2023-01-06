486947015b1af50b75212582f9a80684f975fb12
Naoyuki Ishida <39.ishida@gmail.com>
2023-01-06 14:18:10 -0800

    パス指定に失敗したので指定方法を戻す

--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 plugin :tmp_restart
-# app_root = File.expand_path("../..", __FILE__)
-# bind "unix:#{app_root}/tmp/sockets/puma.sock"
-# ルートのパス指定方法を変更
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+app_root = File.expand_path("../..", __FILE__)
+bind "unix:#{app_root}/tmp/sockets/puma.sock"
