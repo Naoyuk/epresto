95d390e92a002157b29fcd0a4bf1d7dedb264918
Naoyuki Ishida <39.ishida@gmail.com>
2023-01-06 14:03:55 -0800

    tmp/sockets/puma.sockが作成されない問題への対応
    - ソケットのパス指定方法を変更
    - ソケットに直接関係ないと思われるが、Nginxはソケット通信する場合にはPort指定は不要なのでコメントアウト

--- config/puma.rb
+++ config/puma.rb
@@ -3,11 +3,14 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
-port ENV.fetch("PORT") { 3000 }
+# Nginxがソケット通信する場合はPortの指定は不要なのでコメントアウト
+# port ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 plugin :tmp_restart
-app_root = File.expand_path("../..", __FILE__)
-bind "unix:#{app_root}/tmp/sockets/puma.sock"
+# app_root = File.expand_path("../..", __FILE__)
+# bind "unix:#{app_root}/tmp/sockets/puma.sock"
+# ルートのパス指定方法を変更
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
